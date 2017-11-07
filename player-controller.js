32c6221c278e2ec1d7b4551383a8bd10bb3c8729
Josh Eigen <josh.eigen@gmail.com>
2017-11-07 14:18:09 -0800

    possibly

--- player-controller.js
+++ player-controller.js
@@ -1,14 +1,41 @@
-function PlayerController(){
+function PlayerController() {
 
     var loading = true;
     var playerService = new PlayersService(ready);
 
-    function ready(){
+    function ready() {
         loading = false;
 
-        $('some-button').on('click', function(){
+        $('some-button').on('click', function () {
             var teamSF = playerService.getPlayersByTeam("SF");
         })
+    }
+
+    function draw(playerService) {
+
+        var elem = document.getElementById('row');
+        elem.innerHTML = '';
+        var rosterTemplate = '';
+        var player = data.body.players
+
+        for (player in playersData) {
+
+            var playerIndex = playersData[player];
+            rosterTemplate += `
+              <div class="panel panel-default">
+                  <ul id='player-list'>
+                      <div>
+                          <img src="${playerIndex.img}" alt="">
+                          <h4><b>${playerIndex.name}</b></h4>
+                          <h5>${playerIndex.team}</h5>
+                      </div>
+                  </ul>
+              </div>
+              `
+        }
+        elem.innerHTML = rosterTemplate
+
+
     }
 
 
